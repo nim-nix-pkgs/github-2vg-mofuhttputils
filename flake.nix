2f81906a95aad2cf4ffc1d752c87de77a75c97fa
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:59 -0300

    chore: re index github-2vg-mofuhttputils

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."mofuhttputils-master".dir   = "master";
+  inputs."mofuhttputils-master".owner = "nim-nix-pkgs";
+  inputs."mofuhttputils-master".ref   = "master";
+  inputs."mofuhttputils-master".repo  = "mofuhttputils";
+  inputs."mofuhttputils-master".type  = "github";
+  inputs."mofuhttputils-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mofuhttputils-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
